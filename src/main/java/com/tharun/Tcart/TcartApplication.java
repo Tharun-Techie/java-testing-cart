6a1098ba895960db8542aec7ba476b2139483c88
Tharun R <Tharuntechie@gmail.com>
2024-09-14 14:45:19 +0530

    First commit

---
+++ src/main/java/com/tharun/Tcart/TcartApplication.java
@@ -0,0 +1,13 @@
+package com.tharun.Tcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tharun/Tcart/TcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tharun.Tcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
